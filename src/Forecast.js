ace8771c275440fe177cd596970b0316a0f775bf
DubN <111530283+favoritewillow@users.noreply.github.com>
2022-12-02 11:29:08 +0200

    added correct img

--- src/Forecast.js
+++ src/Forecast.js
@@ -16,7 +16,7 @@ export default function Forecast(props) {
       city: response.data.name,
       date: new Date(response.data.dt * 1000) /*last update*/,
       description: response.data.weather[0].description,
-      icon: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       humidity: response.data.main.humidity,
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
